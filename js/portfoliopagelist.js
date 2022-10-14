4043e297d7ddecc7bcff42d7573d1edc25c1fa6a
Guilherme Ferreira <devguirrer@gmail.com>
2022-10-14 01:17:32 -0300

    Footer e SweetAlert

---
+++ js/portfoliopagelist.js
@@ -0,0 +1,47 @@
+function CopiarEmail(){
+
+    const inputValue = 'devguirrer@gmail.com'
+    
+
+    Swal.fire({
+        background: '#000',
+        color: 'white',
+        icon: 'info',
+        title: 'Copie Meu Email Para Entrar Em Contato !!',
+        html: `
+            <input
+            type="text"
+            value="${inputValue}"
+            class="swal2-input"
+            id="email"
+            readonly>`,
+        showCancelButton: true,
+        cancelButtonText: 'Fechar',
+        confirmButtonText: 'Sim, Copiar!!',
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        }
+        
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            let textoCopiado = document.getElementById("email");
+            textoCopiado.select();
+            textoCopiado.setSelectionRange(0, 99999)
+            document.execCommand("copy");
+
+            Swal.fire({
+                
+                icon : 'success',
+                title: 'Email Copiado Com Sucesso !!',
+                background: '#000',
+                color: 'white',
+            }
+          )
+        
+    }})
+
+}
